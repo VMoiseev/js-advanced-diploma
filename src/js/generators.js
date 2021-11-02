701e1e585285645ca7dfd19aa99c8feedc68b874
VMoiseev <e175@inbox.ru>
2021-11-02 10:01:08 +0500

    5. Team Generation

--- src/js/generators.js
+++ src/js/generators.js
@@ -5,10 +5,19 @@
  * @param maxLevel max character level
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
-// export function* characterGenerator(allowedTypes, maxLevel) {
-//   // TODO: write logic here
-// }
+export function* characterGenerator(allowedTypes, maxLevel) {
+  // TODO: write logic here
+  const team = Math.floor(Math.random() * allowedTypes.length);
+  const level = Math.floor(Math.random() * maxLevel) + 1;
+  yield new allowedTypes[team](level);
+}
 
-// export function generateTeam(allowedTypes, maxLevel, characterCount) {
-//   // TODO: write logic here
-// }
+export function generateTeam(allowedTypes, maxLevel, characterCount) {
+  // TODO: write logic here
+  const team = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < characterCount; i++) {
+    team.push(characterGenerator(allowedTypes, maxLevel).next.value);
+  }
+  return team;
+}
